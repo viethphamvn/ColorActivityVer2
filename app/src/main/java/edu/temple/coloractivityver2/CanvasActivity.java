37bd44a52337a3629f7dd5d41c8187532b50f4dc
Viet Pham <tug36669@temple.edu>
2019-10-02 11:19:58 -0400

    Added another activity that receives a color from previous activity and change its background accordingly

--- app/src/main/java/edu/temple/coloractivityver2/CanvasActivity.java
+++ app/src/main/java/edu/temple/coloractivityver2/CanvasActivity.java
@@ -1,8 +1,13 @@
 package edu.temple.coloractivityver2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CanvasActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class CanvasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+
+        ConstraintLayout canvas = findViewById(R.id.canvas);
+
+        Intent receiveColor = getIntent();
+        TextView banner = findViewById(R.id.textView2);
+
+        String msg = receiveColor.getStringExtra("background");
+
+        if (msg != null){
+            banner.setText(msg);
+            banner.setTextColor(Color.WHITE);
+            canvas.setBackgroundColor(Color.parseColor(msg));
+        }
+
     }
 }
